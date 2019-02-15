4cb18b2f30dfe77a33ca4f2af146602d9254896f
ypenn21 <yanni.peng@jukmedia.com>
2019-02-15 12:59:51 -0800

    add system design questions

---
+++ src/com/yanni/test/snapchat/SystemDesignWhiteBoard.java
@@ -0,0 +1,8 @@
+package com.yanni.test.snapchat;
+
+public class SystemDesignWhiteBoard {
+    // two system design questions on top of the algo questions listed
+    // Did this one: Design a system that is fail safe with no single point of failure for a number that increments by one
+    // Didn't figure out good solution for this: Design a system that takes input phone type and run time design a system that schedules different tasks on the phone
+    // scale it and think of features to monetize it.
+}
