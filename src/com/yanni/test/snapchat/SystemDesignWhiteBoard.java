5a4084c5661d9b6ab5c363e33845a8b8031b6602
yanni peng <yanni.peng@chromeriver.com>
2019-03-03 20:40:24 -0800

    add snapchat design idea link

--- src/com/yanni/test/snapchat/SystemDesignWhiteBoard.java
+++ src/com/yanni/test/snapchat/SystemDesignWhiteBoard.java
@@ -5,4 +5,5 @@ public class SystemDesignWhiteBoard {
     // Did this one: Design a system that is fail safe with no single point of failure for a number that increments by one
     // Didn't figure out good solution for this: Design a system that takes input phone type and run time design a system that schedules different tasks on the phone
     // scale it and think of features to monetize it.
+    // design idea for snap chat: https://drive.google.com/file/d/15971Ar8bz5BqWb_n881JdOWDWbgwlwMl/view?usp=sharing
 }
